446156d50490832389e5461cb98a0097c56fe4df
Emiliano Gusberti <emggw@hotmail.com>
2022-12-31 18:06:21 -0300

    Add "/" route & get mtd, pass day & color

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true })); // Required to parse requests
 app.use(express.static("static"));
 
+app.get("/", function (req, res) {
+  const day = [
+    { name: "Sunday", color: "black" },
+    { name: "Monday", color: "red" },
+    { name: "Tuesday", color: "green" },
+    { name: "Wednesday", color: "yellor" },
+    { name: "Thursday", color: "purple" },
+    { name: "Friday", color: "grey" },
+    { name: "Saturday", color: "brown" },
+  ][new Date().getDay()];
 
+  res.render("list", {
+    day: day.name,
+    color: day.color,
+  });
+});
 
 const port = process.env.PORT;
 app.listen(port, function () {
