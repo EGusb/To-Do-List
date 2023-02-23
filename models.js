2ca4f0ec19992e8c1824bad5c5e386f77365bf3a
Emiliano Gusberti <emggw@hotmail.com>
2023-02-23 17:56:46 -0300

    Add itemSchema object

--- models.js
+++ models.js
@@ -4,11 +4,13 @@ mongoose.set("strictQuery", false);
 const dbFullPath = `${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 mongoose.connect(dbFullPath);
 
-const Item = mongoose.model("Item", {
+const itemSchema = {
   name: {
     type: String,
     required: [true, "The item must have a name!"],
   },
-});
+};
+
+const Item = mongoose.model("Item", itemSchema);
 
 exports.Item = Item;
