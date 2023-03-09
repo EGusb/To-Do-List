2bd9ba45fff00f0f79ccc9e360390d620eb929a9
Emiliano Gusberti <emggw@hotmail.com>
2023-03-09 20:07:18 -0300

    Chain methods

--- app.js
+++ app.js
@@ -33,47 +33,48 @@ function renderErrorPage(res, err, statusCode, msg = "ERROR") {
   });
 }
 
-app.get("/", function (req, res) {
-  List.find({}, function (err, docs) {
-    if (err) {
-      renderErrorPage(res, err, 500);
-    } else {
-      res.render("home", {
-        lists: docs,
-      });
-    }
-  });
-});
-
-app.post("/", function (req, res) {
-  const newListName = req.body.newListName;
-  const newListSlug = slugify(newListName);
-
-  List.findOne({ slug: newListSlug }, function (err, list) {
-    if (err) {
-      renderErrorPage(res, err, 500);
-    } else {
-      if (list) {
-        res.redirect("/");
+app
+  .route("/")
+  .get(function (req, res) {
+    List.find({}, function (err, docs) {
+      if (err) {
+        renderErrorPage(res, err, 500);
       } else {
-        List.create(
-          {
-            name: newListName,
-            slug: newListSlug,
-            items: [],
-          },
-          function (err1, doc) {
-            if (err1) {
-              renderErrorPage(res, err1, 500);
-            } else {
-              res.redirect("/");
-            }
-          }
-        );
+        res.render("home", {
+          lists: docs,
+        });
       }
-    }
+    });
+  })
+  .post(function (req, res) {
+    const newListName = req.body.newListName;
+    const newListSlug = slugify(newListName);
+
+    List.findOne({ slug: newListSlug }, function (err, list) {
+      if (err) {
+        renderErrorPage(res, err, 500);
+      } else {
+        if (list) {
+          res.redirect("/");
+        } else {
+          List.create(
+            {
+              name: newListName,
+              slug: newListSlug,
+              items: [],
+            },
+            function (err1, doc) {
+              if (err1) {
+                renderErrorPage(res, err1, 500);
+              } else {
+                res.redirect("/");
+              }
+            }
+          );
+        }
+      }
+    });
   });
-});
 
 app.post("/delete", function (req, res) {
   const checkedListId = req.body.listCheckBox;
@@ -102,43 +103,44 @@ app.get("/:listSlug", function (req, res) {
   });
 });
 
-app.get("/:listSlug/edit", function (req, res) {
-  const listSlug = req.params.listSlug;
+app
+  .route("/:listSlug/edit")
+  .get(function (req, res) {
+    const listSlug = req.params.listSlug;
 
-  List.findOne({ slug: listSlug }, function (err, list) {
-    if (err) {
-      renderErrorPage(res, err, 500);
-    } else {
-      if (list) {
-        res.render("list-edit", { list: list });
-      } else {
-        renderErrorPage(res, err, 404, "Page not found.");
-      }
-    }
-  });
-});
-
-app.post("/:listSlug/edit", function (req, res) {
-  const newListName = req.body.newListName;
-  const listId = req.body.listId;
-  const newSlug = slugify(newListName);
-
-  List.findOneAndUpdate(
-    { _id: listId },
-    { name: newListName, slug: newSlug },
-    function (err, list) {
+    List.findOne({ slug: listSlug }, function (err, list) {
       if (err) {
         renderErrorPage(res, err, 500);
       } else {
         if (list) {
-          res.redirect(`/${newSlug}`);
+          res.render("list-edit", { list: list });
         } else {
-          res.redirect("/");
+          renderErrorPage(res, err, 404, "Page not found.");
         }
       }
-    }
-  );
-});
+    });
+  })
+  .post(function (req, res) {
+    const newListName = req.body.newListName;
+    const listId = req.body.listId;
+    const newSlug = slugify(newListName);
+
+    List.findOneAndUpdate(
+      { _id: listId },
+      { name: newListName, slug: newSlug },
+      function (err, list) {
+        if (err) {
+          renderErrorPage(res, err, 500);
+        } else {
+          if (list) {
+            res.redirect(`/${newSlug}`);
+          } else {
+            res.redirect("/");
+          }
+        }
+      }
+    );
+  });
 
 app.post("/:listSlug/item/add", function (req, res) {
   const newItemName = req.body.newItemName;
